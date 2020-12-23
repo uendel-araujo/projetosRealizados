b298051512cc13871db3e4903771cdaaa65b7290
Uendel Araujo <dendellaraujo@gmail.com>
2020-12-23 11:16:56 -0300

    Finalização do etapas.js e inicio da criação do script.js

---
+++ ProjectUrnaEletronicaEmJs/assets/js/script.js
@@ -0,0 +1,6 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let numeros = document.querySelector('.d-1-3');
+let descricao = document.querySelector('.d-1-4');
+let lateralDireita = document.querySelector('.d-1-right');
+let aviso = document.querySelector('.d-2');
