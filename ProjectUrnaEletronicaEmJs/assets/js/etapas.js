fd53cd17cd175a613d1ef7a3770275acc149d7a3
Uendel Araujo <dendellaraujo@gmail.com>
2020-12-23 11:05:28 -0300

    Adicição do JSON de etapas.js, função dos candidados em JS

---
+++ ProjectUrnaEletronicaEmJs/assets/js/etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano de Tal',
+                partido: 'ABC',
+                fotos: [
+                    {url: './assets/images/38111.jpg', legenda: 'Vereador'}
+                ]
+            },
+
+            {
+                numero: 77222,
+                nome: 'Beltrano das Flores',
+                partido: 'DEF',
+                fotos: [
+                    {url: './assets/images/77222.jpg', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 84,
+                nome: 'Ciclano das Borboletas',
+                partido: 'ABC',
+                vice: 'Florentina dos Morcegos',
+                fotos: [
+                    {url: './assets/images/84.jpg', legenda: 'Prefeito'},
+                    {url: './assets/images/84_2.jpg', legenda: 'Vice-Prefeito'}
+                ]
+            },
+            
+            {
+                numero: 99,
+                nome: 'Coscova do Pacu',
+                partido: 'DEF',
+                vice: 'Neka Do Litrão',
+                fotos: [
+                    {url: './assets/images/99.jpg', legenda: 'Prefeito'},
+                    {url: './assets/images/99_2.jpg', legenda: 'Vice-Prefeito'}
+                ]
+            },
+        ]
+    }
+];
