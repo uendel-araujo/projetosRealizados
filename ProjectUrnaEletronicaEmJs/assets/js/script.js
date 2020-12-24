ee81755170a621128c05d31c60e1c12fbf0cdd38
Uendel Araujo <dendellaraujo@gmail.com>
2020-12-24 09:55:54 -0300

    Adicionada funções especificas e de controle no script.JS

--- ProjectUrnaEletronicaEmJs/assets/js/script.js
+++ ProjectUrnaEletronicaEmJs/assets/js/script.js
@@ -1,3 +1,4 @@
+/** VARIÁVEIS DE CONTROLE DE INTERFACE */
 let seuVotoPara = document.querySelector('.d-1-1 span');
 let cargo = document.querySelector('.d-1-2 span');
 let numeros = document.querySelector('.d-1-3');
@@ -5,8 +6,46 @@ let descricao = document.querySelector('.d-1-4');
 let lateralDireita = document.querySelector('.d-1-right');
 let aviso = document.querySelector('.d-2');
 
+/** VARIÁVEIS DE CONTROLE DE AMBIENTE */
+let etapaAtual = 0;
+let numero = '';
+
+/** FUNÇÕES ESPECÍFICAS */
+function comecarEtapa() {
+    let etapa = etapas[etapaAtual];
+
+    let numeroHtml = '';
+    
+    for(let i=0; i< etapa.numeros; i++) {
+        if( i === 0 ) {
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+            numeroHtml += '<div class="numero"></div>';
+        }
+    }
+
+    seuVotoPara.style.display = 'none';
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateralDireita.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+}
+
+function atualizaInterface() {
+
+}
+
+/** FUNÇÕES DE CONTROLE */
 function clicou(n) {
-    alert(`Clicou em ${n}`);
+    let elNumero = document.querySelector('.numero.pisca');
+    if(elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        elNumero.nextElementSibling.classList.add('pisca');
+    }
 }
 
 function branco() {
@@ -20,3 +59,6 @@ function corrige() {
 function confirma() {
     alert(`Clicou em CONFIRMA`);
 }
+
+/** CHAMADAS DE INICIALIZAÇÃO DAS FUNÇÕES */
+comecarEtapa();
